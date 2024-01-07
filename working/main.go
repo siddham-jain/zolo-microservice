27ffd4b9b0239da6d76d803ec9188ad42d7b9c61
Siddham Jain <siddhamjain224@gmail.com>
2024-01-07 17:44:49 +0530

    Updated http request error handling

--- working/main.go
+++ working/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "net/http"
-
+import (
+	"net/http"
+	"fmt"
+)
 func main(){
+	server := &http.Server{
+		Addr: ":5050",
+		Handler: http.HandlerFunc(basicHandler),
+	}
 
-	http.ListenAndServe(":5050", nil)
+	err := server.ListenAndServe();
+
+	if err != nil {
+		fmt.Println("failed to listen and serve", err);
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Hello World")); 
 }
